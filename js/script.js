67a8b1a640d5db323e7ff6acb423d8183bdeda5a
AlessiaOsman <alessia.osman@gmail.com>
2024-01-12 17:38:22 +0100

    created a div for number and a random number for each

--- js/script.js
+++ js/script.js
@@ -11,3 +11,30 @@ const fourthNumberInput = document.getElementById('fourth-input')
 const fifthNumberInput = document.getElementById('fifth-input')
 const resultElement = document.getElementById('guessed-numbers')
 
+// Variabili iniziali
+
+const totalNumbers = 5;
+
+// Creo le mie funzioni
+
+const createNumberContainer = () => {
+    const numberContainer = document.createElement('div')
+    numberContainer.className = `w-25 text-center fw-bold fs-1`
+
+    return numberContainer
+    
+}
+
+
+for (let i = 1; i <= totalNumbers; i++) {
+
+    let randomNumber = Math.floor(Math.random()*100)+1
+
+    const cpuNumberContainer = createNumberContainer()
+    numbersContainer.appendChild(cpuNumberContainer)
+    cpuNumberContainer.innerText = randomNumber
+
+}
+
+
+
