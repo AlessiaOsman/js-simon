16f2564205ffd56e2c968efab12cb08a8475f51d
AlessiaOsman <alessia.osman@gmail.com>
2024-01-12 16:54:24 +0100

    get element

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// Recupero elementi
+
+const title = document.getElementById('title')
+const numbersContainer = document.getElementById('cpu-numbers')
+const countdownElement = document.getElementById('countdown')
+const formElement = document.getElementById('form')
+const firstNumberInput = document.getElementById('first-input')
+const secondNumberInput = document.getElementById('second-input')
+const thirdNumberInput = document.getElementById('third-input')
+const fourthNumberInput = document.getElementById('fourth-input')
+const fifthNumberInput = document.getElementById('fifth-input')
+const resultElement = document.getElementById('guessed-numbers')
+
